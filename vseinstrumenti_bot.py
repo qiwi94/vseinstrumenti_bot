114fe0ddfe14f5eabbe5538e2a4f0b2ea5699604
qiwi94 <121112471+qiwi94@users.noreply.github.com>
2024-07-08 17:57:36 +0300

    Update vseinstrumenti_bot.py

--- vseinstrumenti_bot.py
+++ vseinstrumenti_bot.py
@@ -2,7 +2,7 @@ import openpyxl
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
-TOKEN = '7475740177:AAEjeLBqPK4j8h6KMVw8nKMEAoTSMqJPsNs' 
+TOKEN = '74w8nKMEAoTSMqJPsNs' 
 
 # Загрузка книги Excel
 try:
